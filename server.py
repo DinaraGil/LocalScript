e79b8ca37d96e15b50b2ddeec50cecd04843b8ac
dinara <dinaragig03@gmail.com>
2026-04-10 15:22:53 +0300

    feat: add prompt handler

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+from litellm import completion
+
+MODEL = "ollama_chat/qwen2.5:3b"
+API_BASE = "http://localhost:11434"
+
 
 app = FastAPI()
 
@@ -9,7 +14,16 @@ class Prompt(BaseModel):
 
 @app.post("/generate")
 def hello(prompt: Prompt):
-    return {"your prompt": f"{prompt.prompt}"}
+    response = completion(
+        model=MODEL,
+        messages=[
+            {"role": "user", "content": prompt.prompt}
+        ],
+        api_base=API_BASE,
+        request_timeout=120,
+    )
+    return response.choices[0].message.content
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
