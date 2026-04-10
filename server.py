baf31f0edddc9d26b485ca2ce61c1bdfa22b8ea0
Waraidako <waraidako.god@gmail.com>
2026-04-10 16:41:35 +0300

    Implemented system prompt & message history

--- server.py
+++ server.py
@@ -3,26 +3,72 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from litellm import completion
 
-MODEL = "ollama_chat/qwen2.5:3b"
+MODEL = "ollama_chat/codellama:7b"
 API_BASE = "http://localhost:11434"
+SYSTEM_PROMPT = '''
+You are an AI agent that generates working Lua code from natural language tasks.
 
+Understand the request and ask short clarifying questions if needed.
+Produce correct, runnable Lua code and check it for obvious mistakes.
+Help the user iteratively improve the solution.
+
+Be concise. Avoid long explanations.
+If information is missing, ask instead of guessing.
+Use code blocks for Lua.
+Explanations: 1 short sentence maximum.
+'''
+MAX_CONTEXT_LENGTH = 4096
 
 app = FastAPI()
 
 class Prompt(BaseModel):
     prompt: str
 
+defaultContext = [
+            { "role": "system", "content": SYSTEM_PROMPT },
+]
+
+
+
+contextLength = len(SYSTEM_PROMPT)
+
+class Context:
+    def __init__(self):
+        self.contextLength = len(SYSTEM_PROMPT)
+        self.contextMessages = [{ "role": "system", "content": SYSTEM_PROMPT }]
+        self.defaultContext = [{ "role": "system", "content": SYSTEM_PROMPT }]
+    
+
+
 @app.post("/generate")
 def hello(prompt: Prompt):
+    ctx = Context()
+
+    contextResetFlag = False
+
+    promptLength = len(prompt.prompt)
+    if ctx.contextLength + promptLength > MAX_CONTEXT_LENGTH:
+        ctx.contextMessages = defaultContext
+        ctx.contextLength = len(SYSTEM_PROMPT)
+        contextResetFlag = True
+
+    ctx.contextLength += promptLength
+    ctx.contextMessages.append({ "role": "user", "content": prompt.prompt })
     response = completion(
         model=MODEL,
-        messages=[
-            {"role": "user", "content": prompt.prompt}
-        ],
-        api_base=API_BASE,
-        request_timeout=120,
+        messages=ctx.contextMessages,
+        api_base = API_BASE,
+        request_timeout = 120,
     )
-    return response.choices[0].message.content
+
+    contextResponse = response.choices[0].message.content
+    if contextResetFlag:
+        contextResponse = "WARNING: CONTEXT HAS BEEN RESET\n\n" + contextResponse
+
+    ctx.contextMessages.append({ "role": "assistant", "content": contextResponse})
+
+    return contextResponse
+    # return { prompt.prompt }
 
 
 if __name__ == "__main__":
