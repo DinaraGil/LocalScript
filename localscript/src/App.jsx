203377cfce77711791744acffc4d4880e7b1705c
Waraidako <waraidako.god@gmail.com>
2026-04-12 18:38:49 +0300

    Added frontend bootstrap

---
+++ localscript/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LocalLLMFrontend from './components/LocalLLMFrontend'
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>PISS</h1>
+      <LocalLLMFrontend />
+    </div>
+    
+  )
+}
+
+export default App
